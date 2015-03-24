8dafe44c6b2e582ed210a4954dec32b8a97ba4c2
David Matins <dmatins@unm.edu>
2015-03-23 19:57:11 -0600

    start screen tune up

--- cs351Project2_Tyler_Tim_David/src/main/StartScreen.java
+++ cs351Project2_Tyler_Tim_David/src/main/StartScreen.java
@@ -4,6 +4,7 @@ import gui.ColorsAndFonts;
 import main.Game;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -15,7 +16,10 @@ import java.awt.event.ActionListener;
  */
 public class StartScreen extends JPanel
 {
+  JPanel textHolder;
   JButton start;
+  JTextArea version;
+  JTextArea text;
 
   /**
    * Initializes a JPanel for the start screen with graphics to be added later and cool stuff
@@ -32,11 +36,35 @@ public class StartScreen extends JPanel
     setLocation(0,0);
     setSize(frameWidth, frameHeight);
 
-    setBackground(ColorsAndFonts.OCEANS);
+    setBackground(new Color(0x1A1A1A));
 
     start = new JButton("START");
     start.setHorizontalAlignment(SwingConstants.RIGHT);
     start.setFocusable(false);
+
+    textHolder = new JPanel();
+    textHolder.setLayout(new BoxLayout(textHolder, BoxLayout.PAGE_AXIS));
+
+    text = new JTextArea();
+    text.setBackground(new Color(0x333333));
+    text.setPreferredSize(new Dimension(500, 500));
+    text.setFont(ColorsAndFonts.GUI_FONT);
+    text.setForeground(Color.white);
+    text.setText(" * \"World Food Production and Land Management\" version 1.2 developed by: \n" +
+        "   Tyler Lynch, Timothy Chavez, and David Matins \n" +
+        "   with a version 1.1 foundation by: Winston Wriley and David Ringo \n\n" +
+        "TODO: why the game was made \n\n" +
+        "TODO: what the game models \n\n" +
+        "TODO: how to play the game");
+    text.setEditable(false);
+
+    version = new JTextArea();
+    version.setBackground(new Color(0x800000));
+    version.setPreferredSize(new Dimension(125,20));
+    version.setFont(ColorsAndFonts.GUI_FONT);
+    version.setForeground(Color.white);
+    version.setText(" WFPaLM Version 1.2");
+    version.setEditable(false);
 
     start.addActionListener(new ActionListener()
       {
@@ -48,5 +76,9 @@ public class StartScreen extends JPanel
       }
     );
     add(start);
+    textHolder.add(version);
+    textHolder.add(text);
+    add(textHolder);
+
   }
 }
