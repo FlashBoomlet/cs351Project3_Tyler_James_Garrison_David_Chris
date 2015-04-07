e67b9cb790f1118f47a3f9c6ac7c34545b7054f2
David Matins <dmatins@unm.edu>
2015-04-06 23:47:42 -0600

    csv error messaging

--- cs351Project2_Tyler_Tim_David/src/IO/CountryCSVParser.java
+++ cs351Project2_Tyler_Tim_David/src/IO/CountryCSVParser.java
@@ -5,6 +5,8 @@ import model.CountryData;
 import model.Region;
 import model.World;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -102,46 +104,52 @@ public class CountryCSVParser {
     if( atributes.length >= 33 )
     {
       String country = atributes[0];
-      if( country.length() <= 0 ) { /*Error*/ }
+      if( country.length() <= 0 ) {
+        errors("Country Name null ",atributes, 0, counter);
+      }
       currentRegion = findCurrentRegion(country);
       if( currentRegion != null )
       {
         data = new CountryData();
         data.setPopulation( Double.parseDouble(atributes[1]) );
-        if( Double.parseDouble(atributes[1]) <= 0 ) { /*Error*/ }
-        data.setMedianAge( Double.parseDouble(atributes[2]) );
-        data.setBirthRate( Double.parseDouble(atributes[3]) );
-        data.setMortality( Double.parseDouble(atributes[4]) );
-        data.setMigration( Double.parseDouble(atributes[5]) );
-        data.setUndernourish( Double.parseDouble(atributes[6]) );
+        if( Double.parseDouble(atributes[1]) <= 0 ) {
+          errors("Population is less than or equal to 0.",atributes, 1, counter);
+        }
+        data.setMedianAge(Double.parseDouble(atributes[2]));
+        data.setBirthRate(Double.parseDouble(atributes[3]));
+        data.setMortality(Double.parseDouble(atributes[4]));
+        data.setMigration(Double.parseDouble(atributes[5]));
+        data.setUndernourish(Double.parseDouble(atributes[6]));
 
-        data.setCornProduction( Double.parseDouble(atributes[7]) );
-        data.setCornExports( Double.parseDouble(atributes[8]) );
-        data.setCornImports( Double.parseDouble(atributes[9]) );
-        data.setCornLand( Double.parseDouble(atributes[10]) );
+        data.setCornProduction(Double.parseDouble(atributes[7]));
+        data.setCornExports(Double.parseDouble(atributes[8]));
+        data.setCornImports(Double.parseDouble(atributes[9]));
+        data.setCornLand(Double.parseDouble(atributes[10]));
 
-        data.setWheatProduction( Double.parseDouble(atributes[11]) );
-        data.setWheatExports( Double.parseDouble(atributes[12]) );
-        data.setWheatImports( Double.parseDouble(atributes[13]) );
-        data.setWheatLand( Double.parseDouble(atributes[14]) );
+        data.setWheatProduction(Double.parseDouble(atributes[11]));
+        data.setWheatExports(Double.parseDouble(atributes[12]));
+        data.setWheatImports(Double.parseDouble(atributes[13]));
+        data.setWheatLand(Double.parseDouble(atributes[14]));
 
-        data.setRiceProduction( Double.parseDouble(atributes[15]) );
-        data.setRiceExports( Double.parseDouble(atributes[16]) );
-        data.setRiceImports( Double.parseDouble(atributes[17]) );
-        data.setRiceLand( Double.parseDouble(atributes[18]) );
+        data.setRiceProduction(Double.parseDouble(atributes[15]));
+        data.setRiceExports(Double.parseDouble(atributes[16]));
+        data.setRiceImports(Double.parseDouble(atributes[17]));
+        data.setRiceLand(Double.parseDouble(atributes[18]));
 
-        data.setSoyProduction( Double.parseDouble(atributes[19]) );
-        data.setSoyExports( Double.parseDouble(atributes[20]) );
-        data.setSoyImports( Double.parseDouble(atributes[21]) );
-        data.setSoyLand( Double.parseDouble(atributes[22]) );
+        data.setSoyProduction(Double.parseDouble(atributes[19]));
+        data.setSoyExports(Double.parseDouble(atributes[20]));
+        data.setSoyImports(Double.parseDouble(atributes[21]));
+        data.setSoyLand(Double.parseDouble(atributes[22]));
 
-        data.setOtherProduction( Double.parseDouble(atributes[23]) );
-        data.setOtherExports( Double.parseDouble(atributes[24]) );
-        data.setOtherImports( Double.parseDouble(atributes[25]) );
-        data.setOtherLand( Double.parseDouble(atributes[26]) );
+        data.setOtherProduction(Double.parseDouble(atributes[23]));
+        data.setOtherExports(Double.parseDouble(atributes[24]));
+        data.setOtherImports(Double.parseDouble(atributes[25]));
+        data.setOtherLand(Double.parseDouble(atributes[26]));
 
         data.setLandArea( Double.parseDouble(atributes[27]) );
-        if( Double.parseDouble(atributes[27]) <= 0 ) { /*Error*/ }
+        if( Double.parseDouble(atributes[27]) <= 0 ) {
+          errors("Land area is less than or equal to 0.",atributes, 27, counter);
+        }
         data.setArableOpen( Double.parseDouble(atributes[28]) );
         data.setOrganic( Double.parseDouble(atributes[29]) );
         data.setConventional( Double.parseDouble(atributes[30]) );
@@ -154,7 +162,35 @@ public class CountryCSVParser {
         currentRegion.setCountryData(data);
       }
     }
+    else{
+      JOptionPane.showMessageDialog(null, "Not enough data in csv in row: "+ counter, "Error in CSV", JOptionPane.ERROR_MESSAGE);
+    }
 
+  }
+
+  public void errors(String msg, String[] atributes, int col, int row){
+    JFrame frame = new JFrame();
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    JTextArea errorMsg = new JTextArea("Error in csv column: "+col+" row: "+row);
+//    String rowData[][] = new String[1][atributes.length];
+//    rowData[0] = atributes;
+//    System.out.println(columnNames);
+//    System.out.println(rowData);
+//    JTable table = new JTable(rowData, columnNames);
+
+    //JScrollPane scrollPane = new JScrollPane(table);
+    JButton save = new JButton("Save");
+    save.addActionListener(new java.awt.event.ActionListener() {
+      public void actionPerformed(java.awt.event.ActionEvent evt) {
+        JOptionPane.showMessageDialog(null, "Save successful!", "INFO: ", JOptionPane.INFORMATION_MESSAGE);
+      }
+    });
+    frame.add(errorMsg, BorderLayout.PAGE_START);
+   // frame.add(scrollPane, BorderLayout.CENTER);
+    frame.add(save,BorderLayout.PAGE_END);
+    frame.setSize(300, 150);
+    frame.setVisible(true);
   }
 
   /**
