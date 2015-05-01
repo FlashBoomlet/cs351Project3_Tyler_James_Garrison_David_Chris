db191bb4b1f4cde8200b77c3bc56b33b31f54099
Tyler Lynch <lyncht@unm.edu>
2015-05-01 01:18:38 -0600

    More structure and outlining of my idea for the start screen. See comments for what will go where if you would really like to know. It is supper hacked up hahah

--- src/main/StartScreen/BeginScreen.java
+++ src/main/StartScreen/BeginScreen.java
@@ -1,9 +1,15 @@
 package main.StartScreen;
 
+import gui.ColorsAndFonts;
+
 import javax.swing.*;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import static javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER;
 
 /**
  * Container for the country selection and preview
@@ -16,6 +22,7 @@ class BeginScreen extends JPanel implements ActionListener
   private JButton beginGame;
   private int frameWidth;
   private int frameHeight;
+
 
   /*
    * Components for the begin screen
@@ -96,26 +103,49 @@ class BeginScreen extends JPanel implements ActionListener
    */
   private class CountrySelect extends JPanel
   {
+    public final EmptyBorder PADDING_BORDER = new EmptyBorder(2, 2, 2, 2);
+    // Null color basically
+    private final Color BORDER_COL = new Color(0.0f,0.0f,0.0f,0.0f);
+
     private CountrySelect()
     {
       super();
-      setOpaque(true);
+      setOpaque(false);
       setBackground(Color.RED);
-      setBounds(0,0,getWidth(),getHeight());
 
       /*
        * Measurements for the JScrollPane
        */
-      int width = (int) (getWidth()*(.75));
-      int height = (int) (getHeight()*(.75));
-      int x = (getWidth()-(width/2));
-      int y = (getHeight()-(height/2));
-      JScrollPane scrollPane = new JScrollPane();
-      scrollPane.setLocation(x, y);
-      scrollPane.setSize(width, height);
-      scrollPane.setOpaque(true);
-      scrollPane.setBackground(Color.CYAN);
-      add(scrollPane);
+      int width = (int) ((frameWidth/2)*(.5));
+      int height = (int) (frameHeight*(.75));
+      int x = ((frameWidth/4)-(width/2));
+      int y = (frameHeight-height)/4;
+
+      /*
+       * Taking hacking of the positioning to a whole new level.
+       */
+      setBorder(new CompoundBorder(BorderFactory.createMatteBorder(y, x, frameHeight-(height+y),(frameWidth/2)-(width+x), BORDER_COL), PADDING_BORDER));
+
+
+      JPanel countries = new JPanel();
+      countries.setBackground(Color.CYAN);
+      countries.setPreferredSize(new Dimension(width, height * 5));
+      countries.setLayout(new FlowLayout());
+      countries.add(new JLabel("Use Logic from Chart Key to            "));
+      countries.add(new JLabel( "handle sizing & creating             \n " ));
+      countries.add(new JLabel( " a custom list                  "));
+      countries.add(new JLabel( "               "));
+      for( int i = 0; i < 50; i ++ ) countries.add(new JLabel("Countries will go here"));
+
+      JScrollPane scrollPane = new JScrollPane(countries);
+      scrollPane.setOpaque(false);
+      scrollPane.setLocation(0,0);
+      scrollPane.setPreferredSize(new Dimension(width, height));
+      scrollPane.getVerticalScrollBar().setPreferredSize(new Dimension(10, 10));
+      scrollPane.getHorizontalScrollBar().setPreferredSize(new Dimension(10, 10));
+      //scrollPane.setHorizontalScrollBarPolicy(HORIZONTAL_SCROLLBAR_NEVER);
+      add(scrollPane );
+
     }
   }
 
@@ -127,23 +157,53 @@ class BeginScreen extends JPanel implements ActionListener
     private CountryPreview()
     {
       super();
-      setOpaque(true);
-      setBackground(Color.CYAN);
-      setBounds(0,0,getWidth(),getHeight());
+      setOpaque(false);
+      setLayout(new GridLayout(2,1));
 
       /*
        * Measurements for the JScrollPane
        */
-      int width = (int) (getWidth()*(.75));
-      int height = (int) (getHeight()*(.75));
-      int x = (getWidth()-(width/2));
-      int y = (getHeight()-(height/2));
-      JPanel infoPanel = new JPanel();
-      infoPanel.setLocation(x, y);
-      infoPanel.setSize(width, height);
-      infoPanel.setOpaque(true);
-      infoPanel.setBackground(Color.RED);
-      add(infoPanel);
+      int width = (int) ((getWidth()/2)*(.5));
+      int height = (int) (this.getHeight()*(.5));
+      int x = ((getWidth()/2)-(width/2));
+      int y = ((getWidth()/2)-(height/2));
+
+      /*
+       * Create a mini map view of the country for this area
+       *
+       * Add a constructor for something to this
+       */
+      JPanel miniMap = new JPanel();
+      miniMap.setOpaque(true);
+      miniMap.setBackground(Color.RED);
+      add(miniMap);
+
+      /*
+       * Lower container Only for formatting
+       */
+      JPanel lowerCon = new JPanel();
+      lowerCon.setOpaque(false);
+      lowerCon.setLayout(new GridLayout(1,2));
+
+      /*
+       * Lower Left Container Panel to hold statistics on  your country/character that you choose
+       *
+       * Add a constructor for something to this
+       */
+      JPanel left = new JPanel();
+      left.setBackground( Color.CYAN );
+      lowerCon.add( left );
+
+      /*
+       * Lower right Container Panel to hold pie chars and data on your country/character that you choose
+       *
+       * Add a constructor for something to this
+       */
+      JPanel right = new JPanel();
+      right.setBackground( Color.PINK );
+      lowerCon.add( right );
+
+      add(lowerCon);
     }
   }
 }
